cbaf0e68ff64c1a71d3de831e69bea791afb931c
NexcentriQ <ab3935s.missouristate@outlook.com>
2026-05-23 19:49:03 -0500

    first commit

---
+++ src/main/java/com/app/hiremebot/HiremebotApplication.java
@@ -0,0 +1,13 @@
+package com.app.hiremebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiremebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiremebotApplication.class, args);
+	}
+
+}
